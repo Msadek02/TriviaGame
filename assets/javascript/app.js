ce8a5f7cc85d31048762627dd9731401f97a5bf1
Msadek02 <Thunder_axe_90@hotmail.com>
2018-01-12 13:40:02 -0500

    global variables

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+// we going to set global variables to our game.
+var startScreen;
+var gameHTML;
+var counter = 30;
+var questionArray = [""];
+var answerArray = [[""]]; 
+var correctAnswer = [];
+var questionCounter = 0;
+var selectAnswer;
+var theClock;
+var correctTally = 0;
+var incorrectTally = 0;
+var unansweredTally = 0;
