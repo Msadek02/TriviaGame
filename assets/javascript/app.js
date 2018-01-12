e69e30415f33d5748ad5be39685d70e4e0f08d5e
Msadek02 <Thunder_axe_90@hotmail.com>
2018-01-12 14:29:35 -0500

    start game button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,3 +11,19 @@ var theClock;
 var correctTally = 0;
 var incorrectTally = 0;
 var unansweredTally = 0;
+
+
+$(document).ready(function(){
+
+    // creat a function to intialize and start the screen.
+			function intialScreen (){
+				startScreen = "<p class='text-center main-button-container'><a class='btn btn-primary btn-lg btn-block start-button' href='#' role='button'>Start Button</a></p>";
+				$(".mainArea").html(startScreen);
+			}
+
+			intialScreen();
+
+			$("body").on("click", ".start-button", function(event){
+				event.preventDefault(); 
+			});
+});
